1a21685e9ec7e43eb9f5ce690dc3a9e9466408ac
Michele Rastelli <rashtao@gmail.com>
2020-03-27 18:46:32 +0100

    spark streaming test

--- src/test/java/com/arangodb/spark/java/ArangoSparkJavaWriteTest.java
+++ src/test/java/com/arangodb/spark/java/ArangoSparkJavaWriteTest.java
@@ -1,25 +1,25 @@
 package com.arangodb.spark.java;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 import com.arangodb.ArangoDB;
 import com.arangodb.ArangoDBException;
 import com.arangodb.spark.ArangoSpark;
 import com.arangodb.spark.WriteOptions;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ArangoSparkJavaWriteTest {
 
@@ -31,26 +31,31 @@ public class ArangoSparkJavaWriteTest {
 	@BeforeClass
 	public static void setup() throws IOException {
 		arangoDB = new ArangoDB.Builder().build();
-		
+
 		InputStream configFileStream = ClassLoader.getSystemClassLoader().getResourceAsStream("arangodb.properties");
 		Properties arangoDBProperties = new Properties();
 		arangoDBProperties.load(configFileStream);
-		
+
 		SparkConf conf = new SparkConf(false).setMaster("local").setAppName("test");
-		
+
 		// Set values from arangodb.properties to spark context 
 		arangoDBProperties.forEach((T, U) -> {
-			conf.set((String)T, (String)U);
+			conf.set((String) T, (String) U);
 		});
-		
+
 		sc = new JavaSparkContext(conf);
-		
+
 		try {
 			arangoDB.db(DB).drop();
 		} catch (ArangoDBException e) {
 		}
 		arangoDB.createDatabase(DB);
 		arangoDB.db(DB).createCollection(COLLECTION);
+	}
+
+	@Before
+	public void init() {
+		arangoDB.db(DB).collection(COLLECTION).truncate();
 	}
 
 	@AfterClass
@@ -61,8 +66,7 @@ public class ArangoSparkJavaWriteTest {
 	}
 
 	private void checkDocumentCount(Long count) {
-		assertThat(arangoDB.db(DB).collection(COLLECTION).count().getCount(),
-				is(count));
+		assertThat(arangoDB.db(DB).collection(COLLECTION).count().getCount(), is(count));
 	}
 
 	@Test
@@ -77,6 +81,27 @@ public class ArangoSparkJavaWriteTest {
 		ArangoSpark.save(documents, COLLECTION, new WriteOptions().database(DB));
 
 		checkDocumentCount(100L);
+	}
+
+	@Test
+	public void saveFromSparkStreaming() throws InterruptedException {
+		checkDocumentCount(0L);
+
+		List<TestJavaEntity> docs = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			docs.add(new TestJavaEntity());
+		}
+		Queue<JavaRDD<TestJavaEntity>> q = new LinkedList<>(Collections.singletonList(sc.parallelize(docs)));
+
+		JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.milliseconds(500));
+		jssc.queueStream(q).foreachRDD(rdd -> {
+			ArangoSpark.save(rdd, COLLECTION, new WriteOptions().database(DB));
+		});
+
+		jssc.start();
+		jssc.awaitTerminationOrTimeout(1_000);
+
+		checkDocumentCount(10L);
 	}
 
 }
